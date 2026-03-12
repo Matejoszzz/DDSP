69096b83e75ccf5d5e4fe6fe1bf730b83872370e
Matejoszzz <150344942+Matejoszzz@users.noreply.github.com>
2026-03-12 09:39:25 +0100

    Hopes and prayers

--- project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/tb_Ass1_behav/obj/xsim_1.c
+++ project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/tb_Ass1_behav/obj/xsim_1.c
@@ -47,13 +47,14 @@ extern int main(int, char**);
 extern void execute_13(char*, char *);
 extern void execute_10(char*, char *);
 extern void execute_11(char*, char *);
+extern void execute_12(char*, char *);
 extern void vhdl_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
-funcp funcTab[4] = {(funcp)execute_13, (funcp)execute_10, (funcp)execute_11, (funcp)vhdl_transfunc_eventcallback};
-const int NumRelocateId= 4;
+funcp funcTab[5] = {(funcp)execute_13, (funcp)execute_10, (funcp)execute_11, (funcp)execute_12, (funcp)vhdl_transfunc_eventcallback};
+const int NumRelocateId= 5;
 
 void relocate(char *dp)
 {
-	iki_relocate(dp, "xsim.dir/tb_Ass1_behav/xsim.reloc",  (void **)funcTab, 4);
+	iki_relocate(dp, "xsim.dir/tb_Ass1_behav/xsim.reloc",  (void **)funcTab, 5);
 	iki_vhdl_file_variable_register(dp + 2944);
 	iki_vhdl_file_variable_register(dp + 3000);
 
--- project_1/project_1.sim/sim_1/impl/timing/xsim/tb_ALU_time_impl.v
+++ project_1/project_1.sim/sim_1/impl/timing/xsim/tb_ALU_time_impl.v
@@ -1,7 +1,7 @@
 // Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
-// Date        : Tue Mar 10 11:59:56 2026
+// Date        : Tue Mar 10 13:03:55 2026
 // Host        : Matej running 64-bit major release  (build 9200)
 // Command     : write_verilog -mode timesim -nolib -sdf_anno true -force -file
 //               C:/Users/marti/Documents/GitHub/DDSP/project_1/project_1.sim/sim_1/impl/timing/xsim/tb_ALU_time_impl.v
